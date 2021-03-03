140669c7511c5f31ad9fddd6040c53813abbd0ed
Edson Mallet <edsonmmr@gmail.com>
2021-03-03 16:46:23 -0300

    fix: correct code in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
 import { CodePulse } from '@/components/CodePulse';
 import Layout from '@/components/Layout';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 const Home: React.FC = () => {
-  const [code, setCode] = useState<string>('');
-
   return (
     <>
       <Layout companyLogoSrc="/logos/fluxo_logo.svg">
         <p>Digite o código de acesso da pesquisa para conseguir responder.</p>
         <p>Sua participação é muito importante para sua Empresa</p>
-        <CodePulse onChange={(event) => setCode(event.target.value)} />
+        <CodePulse />
       </Layout>
     </>
   );
